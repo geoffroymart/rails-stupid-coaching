bd438f7965ae7d978222bafdb29153b42a2d935d
Geoffroy Marticou <geoffroy.marticou@gmail.com>
2018-05-21 15:03:28 +0200

    1st exo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = ''
+    if @question == 'I am going to work'
+      then @answer = 'Great !'
+    elsif @question.split().last == '?'
+      then @answer = 'Silly question, get dressed and go to work!'
+    else @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
